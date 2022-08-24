7935bc462a7d0a75c56c89218e7218cab5e1a508
照秋 <zhengyannan.zyn@alibaba-inc.com>
2022-08-24 18:54:45 +0800

    channel

--- MyOwtSignallingChannel.h
+++
@@ -1,6 +0,0 @@
-#ifndef MY_OWT_SIGNALLING_CHANNEL_H
-#define MY_OWT_SIGNALLING_CHANNEL_H
-
-
-
-#endif
---
+++ OwtClient.h
@@ -0,0 +1,12 @@
+#ifndef OWT_CLIENT_H
+#define OWT_CLIENT_H
+
+#include "owt/p2p/p2pclient.h"
+
+using namespace owt::p2p;
+
+class OwtClient: P2PClientObserver {
+
+};
+
+#endif
